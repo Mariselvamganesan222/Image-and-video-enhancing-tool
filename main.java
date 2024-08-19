6ceb9d7faad49c350b18f37330ec93c55a583bb6
Mariselvamganesan222 <143953447+Mariselvamganesan222@users.noreply.github.com>
2024-08-19 23:44:57 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+photo-video-enhancing-platform/
+│
+├── backend/
+│   ├── server.js
+│   ├── routes/
+│   │   └── fileUpload.js
+│   ├── uploads/  (This folder will store uploaded files)
+│   └── package.json
+├── public/
+│   ├── index.html
+│   ├── style.css
+│   └── script.js
+└── .gitignore
+  
